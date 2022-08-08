1e073af6b11c820cfaeaf9b2f773058fb79ae476
rrosas1198 <rrosas1198@gmail.com>
2022-08-07 20:33:41 -0500

    docs(components): vue jsx support added

--- packages/components/.vitebook/config.ts
+++ packages/components/.vitebook/config.ts
@@ -2,18 +2,19 @@ import { clientPlugin, defineConfig } from "@vitebook/client/node";
 import { DefaultThemeConfig, defaultThemePlugin } from "@vitebook/theme-default/node";
 import { vuePlugin } from "@vitebook/vue/node";
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig<DefaultThemeConfig>({
-    include: ["src/**/*.md", "src/**/*.story.vue"],
+    include: ["src/**/*.story.vue"],
     plugins: [
         vuePlugin({ appFile: "App.vue" }),
         clientPlugin(),
         defaultThemePlugin(),
-        vue({ include: /\.(md|vue)/ })
+        vue(),
+        vueJsx()
     ],
     site: {
-        title: "Vitebook",
-        description: "Blazing fast Storybook alternative.",
+        title: "Components",
         theme: {
             remoteGitRepo: {
                 url: "vitebook/vitebook"
