8aba941a2039f8f34d482c2ef7be0ef2a3d6e953
rrosas1198 <rrosas1198@gmail.com>
2022-09-23 23:00:30 -0500

    fix(console): migration runner syntax replaced by new di flow

--- console/src/migration/migration.runner.ts
+++ console/src/migration/migration.runner.ts
@@ -1,9 +1,9 @@
-import { InjectMysql, MysqlService } from "@keep/mysql";
+import { IMysqlService, MysqlService } from "@keep/mysql";
 import { IRunner } from "src/interfaces";
 import MigrationDatabase from "./migration.database.sql";
 
 export class MigrationRunner implements IRunner {
-    constructor(@InjectMysql() private readonly mysqlService: Promise<MysqlService>) {}
+    constructor(@IMysqlService private readonly mysqlService: Promise<MysqlService>) {}
 
     public async execute() {
         const mysqlService = await this.mysqlService;
