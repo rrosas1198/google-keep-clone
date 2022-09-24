0a56cbd90b090dd9246990628609047c911258d7
rrosas1198 <rrosas1198@gmail.com>
2022-09-23 21:25:08 -0500

    feat(validations): added third party validation library

--- packages/validations/src/rules/is-valid-email.rule.ts
+++ packages/validations/src/rules/is-valid-email.rule.ts
@@ -1,9 +1,8 @@
 import { ValidationCodeEnum } from "src/enums";
 import { defineRule } from "src/utils";
-
-const regexp = /^[\w.+-]+@[\w-]+\.[\w-]+(?:[.][\w-]+)*$/;
+import _isValidEmail from "validator/lib/isEmail";
 
 export const isValidEmail = defineRule({
-    func: value => typeof value === "string" && regexp.test(value),
+    func: value => typeof value === "string" && _isValidEmail(value),
     code: ValidationCodeEnum.FIELD_MUST_BE_VALID_EMAIL
 });
